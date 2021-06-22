a1097fab94b9a6bc43949eb04f17b7fee6955ba2
Nikolay Kuleshov <nikolay.kuleshov@moex.com>
2021-06-22 09:45:26 +0300

    Отрефачил на RedisMap.

--- src/main/java/ru/n5y/homework/wk/RedisConfiguration.java
+++ src/main/java/ru/n5y/homework/wk/RedisConfiguration.java
@@ -6,13 +6,26 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.data.redis.support.atomic.RedisAtomicLong;
+import org.springframework.data.redis.support.collections.DefaultRedisMap;
+import org.springframework.data.redis.support.collections.RedisMap;
+import ru.n5y.homework.wk.service.LikeService;
+import ru.n5y.homework.wk.service.PlayerService;
 import ru.n5y.homework.wk.service.redis.LongSerializer;
 import ru.n5y.homework.wk.service.redis.RedisConstants;
+import ru.n5y.homework.wk.service.redis.RedisLikesService;
+import ru.n5y.homework.wk.service.redis.RedisPlayerService;
 
 @Configuration
 public class RedisConfiguration {
+
+  private final RedisConnectionFactory redisConnectionFactory;
+
+  public RedisConfiguration(RedisConnectionFactory redisConnectionFactory) {
+    this.redisConnectionFactory = redisConnectionFactory;
+  }
+
   @Bean
-  RedisTemplate<String, Long> playersTemplate(RedisConnectionFactory redisConnectionFactory) {
+  RedisTemplate<String, Long> likesTemplate() {
     final RedisTemplate<String, Long> redisTemplate = new RedisTemplate<>();
     redisTemplate.setConnectionFactory(redisConnectionFactory);
     redisTemplate.setKeySerializer(new StringRedisSerializer());
@@ -22,8 +35,23 @@ public class RedisConfiguration {
   }
 
   @Bean
-  RedisAtomicLong sequence(RedisConnectionFactory redisConnectionFactory) {
+  RedisAtomicLong sequence() {
     return new RedisAtomicLong(RedisConstants.SEQUENCE_ID, redisConnectionFactory);
+  }
+
+  @Bean
+  RedisMap<String, Long> likesMap() {
+    return new DefaultRedisMap<>(likesTemplate().boundHashOps(RedisConstants.HASH_LIKES));
+  }
+
+  @Bean
+  LikeService redisLikeService() {
+    return new RedisLikesService(likesMap());
+  }
+
+  @Bean
+  PlayerService redisPlayerService() {
+    return new RedisPlayerService(likesMap(), sequence());
   }
 
 }
--- src/main/java/ru/n5y/homework/wk/service/redis/RedisLikesService.java
+++ src/main/java/ru/n5y/homework/wk/service/redis/RedisLikesService.java
@@ -3,31 +3,27 @@ package ru.n5y.homework.wk.service.redis;
 import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
+import org.springframework.data.redis.support.collections.RedisMap;
 import ru.n5y.homework.wk.service.LikeService;
 import ru.n5y.homework.wk.service.NoSuchPlayerException;
 
 /**
  * Реализация {@link LikeService} с использованием {@link RedisTemplate}.
  */
-@Service
 public class RedisLikesService implements LikeService {
   private static final Logger log = LoggerFactory.getLogger(RedisLikesService.class);
-  private final BoundHashOperations<String, String, Long> hashOperations;
+  private final RedisMap<String, Long> likes;
 
-  public RedisLikesService(RedisTemplate<String, Long> redisTemplate) {
-    this.hashOperations = Objects.requireNonNull(redisTemplate, "'redisTemplate' should not be null.")
-        .boundHashOps(RedisConstants.HASH_LIKES);
+  public RedisLikesService(RedisMap<String, Long> likes) {
+    this.likes = Objects.requireNonNull(likes, "'likes' should not be null.");
   }
 
   @Override
   public void like(String playerId) {
-    final Boolean hasKey = hashOperations.hasKey(playerId);
-    if (hasKey != null && hasKey) {
-      final Long likes = hashOperations.increment(playerId, 1L);
-      log.info("Player '{}' has been liked, likes count is '{}'.", playerId, likes);
+    if (likes.containsKey(playerId)) {
+      final Long newLikesCount = likes.increment(playerId, 1L);
+      log.info("Player '{}' has been liked, likes count is '{}'.", playerId, newLikesCount);
     } else {
       log.warn("Cannot find player '{}' -  throw NoSuchPlayerException.", playerId);
       throw new NoSuchPlayerException(String.format("Cannot find player with id '%s'.", playerId));
@@ -37,11 +33,12 @@ public class RedisLikesService implements LikeService {
 
   @Override
   public long getLikes(String playerId) {
-    final Long likes = hashOperations.get(playerId);
-    if (likes == null) {
+    final Long likesCount = likes.get(playerId);
+    if (likesCount == null) {
+      log.warn("Cannot find player '{}' -  throw NoSuchPlayerException.", playerId);
       throw new NoSuchPlayerException(String.format("Cannot find player with id '%s'.", playerId));
     }
-    return likes;
+    return likesCount;
   }
 
 }
